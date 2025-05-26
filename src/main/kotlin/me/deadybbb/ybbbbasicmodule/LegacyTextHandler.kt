b54e13c475686e6aee8ce6a52bc5fb03dcf6b12a
mitsu-wq <bmitsu779@gmail.com>
2025-05-26 23:02:06 +0700

    Add strip formatting function

--- src/main/kotlin/me/deadybbb/ybbbbasicmodule/LegacyTextHandler.kt
+++ src/main/kotlin/me/deadybbb/ybbbbasicmodule/LegacyTextHandler.kt
@@ -3,19 +3,22 @@ package me.deadybbb.ybbbbasicmodule
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.command.CommandSender
 import java.util.regex.Pattern
 
 open class LegacyTextHandler {
     companion object {
-        private val LEGACY_PATTERN: Pattern = Pattern.compile("&[0-9a-fk-or]")
+        private val LEGACY_PATTERN = Pattern.compile("&[0-9a-fk-or]")
+        private val MINI_MESSAGE = MiniMessage.miniMessage()
+        private val PLAIN_TEXT_SERIALIZER = PlainTextComponentSerializer.plainText()
 
         @JvmStatic
         fun parseText(text: String): Component {
             val msg = if (LEGACY_PATTERN.matcher(text).find()) {
                 LegacyComponentSerializer.legacyAmpersand().deserialize(text)
             } else {
-                MiniMessage.miniMessage().deserialize(text)
+                MINI_MESSAGE.deserialize(text)
             }
             return msg
         }
@@ -28,6 +31,12 @@ open class LegacyTextHandler {
         @JvmStatic
         fun sendFormattedMessage(sender: CommandSender, text: Component) {
             sender.sendMessage(text)
+        }
+
+        @JvmStatic
+        fun stripFormatting(text: String): String {
+            val plainText = LEGACY_PATTERN.matcher(text).replaceAll("")
+            return PLAIN_TEXT_SERIALIZER.serialize(parseText(plainText))
         }
     }
 }
